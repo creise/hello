60c9d36846c43874f59c749aea2a0c3fc90366bf
creise <christian.reisenberger@asoss.at>
2018-02-09 23:14:45 +0100

    aaa

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,6 +1,13 @@
 #include<iostream>
+#include <fstream>
+using namespace std;
 
 int main(int argc, char *argv[]){
-   std::cout << "Hello World!111" << std::endl;
+  ofstream myfile;
+  myfile.open ("example.html");
+
+  myfile << "<!DOCTYPE html PUBLIC \"-//IETF//DTD HTML 2.0//EN\"> <HTML> <HEAD> <TITLE> A Small Hello </TITLE> </HEAD> <BODY> <H1>Hi</H1>   <P>This is very minimal \"hello world\" HTML document.</P> </BODY> </HTML>";
+  
+  myfile.close();
    return 0;
 }
