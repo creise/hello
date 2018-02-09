b2273095cabdfdf08883a578e03d40114530f238
creise <christian.reisenberger@asoss.at>
2018-02-09 23:22:22 +0100

    bbbb

--- helloworld.cpp
+++ helloworld.cpp
@@ -9,5 +9,12 @@ int main(int argc, char *argv[]){
   myfile << "<!DOCTYPE html PUBLIC \"-//IETF//DTD HTML 2.0//EN\"> <HTML> <HEAD> <TITLE> A Small Hello </TITLE> </HEAD> <BODY> <H1>Hi</H1>   <P>This is very minimal \"hello world\" HTML document.</P> </BODY> </HTML>";
   
   myfile.close();
+
+  ofstream myfile1;
+  myfile1.open ("example1.html");
+
+  myfile1 << "<!DOCTYPE html PUBLIC \"-//IETF//DTD HTML 2.0//EN\"> <HTML> <HEAD> <TITLE> A Small Hello </TITLE> </HEAD> <BODY> <H1>Hi</H1>   <P>This is very minimal \"hello world\" HTML document.</P> </BODY> </HTML>";
+  
+  myfile1.close();
    return 0;
 }
