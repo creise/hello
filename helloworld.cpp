674fe3fbd258cebbbc403ce56343c9069c1d60b2
creise <christian.reisenberger@asoss.at>
2018-02-11 14:15:59 +0100

    asfdd

--- helloworld.cpp
+++ helloworld.cpp
@@ -15,6 +15,6 @@ int main(int argc, char *argv[]){
 
 	myfile1 << "<!DOCTYPE html PUBLIC \"-//IETF//DTD HTML 2.0//EN\"> <HTML> <HEAD> <TITLE> A Small Hello </TITLE> </HEAD> <BODY> <H1>Hi</H1>   <P>This is very minimal \"hello world\" HTML document.</P> </BODY> </HTML>";
 
-	myfile1.close();
+	myfile1.close();sdfds
 	return 0;
 }
