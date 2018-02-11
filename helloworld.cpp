6be8b0eb279c6e39a1d285fc3cbf17b818303d50
creise <christian.reisenberger@asoss.at>
2018-02-11 14:20:01 +0100

    sadsad

--- helloworld.cpp
+++ helloworld.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 int main(int argc, char *argv[]){
+	int i;
 	ofstream myfile;
 	myfile.open ("example.html");
 
@@ -15,6 +16,6 @@ int main(int argc, char *argv[]){
 
 	myfile1 << "<!DOCTYPE html PUBLIC \"-//IETF//DTD HTML 2.0//EN\"> <HTML> <HEAD> <TITLE> A Small Hello </TITLE> </HEAD> <BODY> <H1>Hi</H1>   <P>This is very minimal \"hello world\" HTML document.</P> </BODY> </HTML>";
 
-	myfile1.close();sdfds
+	myfile1.close();
 	return 0;
 }
