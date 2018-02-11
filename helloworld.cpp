673cd321578085bfb833de3ddf6664e8d72b501f
creise <christian.reisenberger@asoss.at>
2018-02-11 12:36:06 +0100

    pretty

--- helloworld.cpp
+++ helloworld.cpp
@@ -3,18 +3,18 @@
 using namespace std;
 
 int main(int argc, char *argv[]){
-  ofstream myfile;
-  myfile.open ("example.html");
+	ofstream myfile;
+	myfile.open ("example.html");
 
-  myfile << "<!DOCTYPE html PUBLIC \"-//IETF//DTD HTML 2.0//EN\"> <HTML> <HEAD> <TITLE> A Small Hello </TITLE> </HEAD> <BODY> <H1>Hi</H1>   <P>This is very minimal \"hello world\" HTML document.</P> </BODY> </HTML>";
-  
-  myfile.close();
+	myfile << "<!DOCTYPE html PUBLIC \"-//IETF//DTD HTML 2.0//EN\"> <HTML> <HEAD> <TITLE> A Small Hello </TITLE> </HEAD> <BODY> <H1>Hi</H1>   <P>This is very minimal \"hello world\" HTML document.</P> </BODY> </HTML>";
 
-  ofstream myfile1;
-  myfile1.open ("example1.html");
+	myfile.close();
 
-  myfile1 << "<!DOCTYPE html PUBLIC \"-//IETF//DTD HTML 2.0//EN\"> <HTML> <HEAD> <TITLE> A Small Hello </TITLE> </HEAD> <BODY> <H1>Hi</H1>   <P>This is very minimal \"hello world\" HTML document.</P> </BODY> </HTML>";
-  
-  myfile1.close();
-   return 0;
+	ofstream myfile1;
+	myfile1.open ("example1.html");
+
+	myfile1 << "<!DOCTYPE html PUBLIC \"-//IETF//DTD HTML 2.0//EN\"> <HTML> <HEAD> <TITLE> A Small Hello </TITLE> </HEAD> <BODY> <H1>Hi</H1>   <P>This is very minimal \"hello world\" HTML document.</P> </BODY> </HTML>";
+
+	myfile1.close();
+	return 0;
 }
